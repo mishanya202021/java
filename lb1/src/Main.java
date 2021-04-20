68d4501fdea859bce2e015fa917e1946a7776622
ev9en4ik <82829821+ev9en4ik@users.noreply.github.com>
2021-04-20 20:24:15 +0300

    Ev9en4ik

--- lb1/src/Main.java
+++ lb1/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println();
+        System.out.println("Hello word!");
+        System.out.println("fdsf");
     }
 }
